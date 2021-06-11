dafc55592b9fc7a0c08a72edc28836d78f003985
Maadh <maadh@pm.me>
2021-06-11 11:35:01 +0500

    FEAT: Improved styling of lottie grid and tiles

--- components/LottiesGrid.tsx
+++ components/LottiesGrid.tsx
@@ -3,7 +3,7 @@ import { LottieResponse } from "../types/LottieResponse";
 
 export default function LottiesGrid({ data }: LottieResponse) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 pb-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-10">
       {data.page.edges.map((lottie) => (
         <LottieTile
           assetUrl={lottie.node.assetUrl}
